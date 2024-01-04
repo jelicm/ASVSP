bfeb99ebb307ffd25abfa2807ea5e63300ecafef
jelicm <jelicm2000@gmail.com>
2024-01-04 20:44:38 +0100

    wireshark producer added

--- real-time/consumer/consumer.py
+++ real-time/consumer/consumer.py
@@ -1,2 +1,33 @@
+# docker cp consumer.py spark-master:consumer.py
+# /spark/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1 consumer.py
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode, split
 
-print("Consumer works")
+def quiet_logs(sc):
+    logger = sc._jvm.org.apache.log4j
+    logger.LogManager.getLogger("org").setLevel(logger.Level.ERROR)
+    logger.LogManager.getLogger("akka").setLevel(logger.Level.ERROR)
+
+spark = SparkSession.builder \
+    .appName("SSS - Kafka String Consumer") \
+    .getOrCreate()
+
+quiet_logs(spark)
+
+rez = spark \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "kafka1:19092") \
+    .option("subscribe", "network-data") \
+    .load()
+
+rez = rez.selectExpr("CAST(value AS STRING) as value")
+
+query = rez \
+    .writeStream \
+    .outputMode("append") \
+    .format("console") \
+    .option("truncate", "false") \
+    .start()
+
+query.awaitTermination()
--- real-time/producer/producer.py
+++ real-time/producer/producer.py
@@ -1,3 +1,37 @@
+import pyshark
+from kafka import KafkaProducer
+import kafka.errors
+import time
+
+KAFKA_TOPIC_NAME = "network-data"
+KAFKA_BROKER = "kafka1:19092"
 
 
-print("Producer works")
+if __name__ == "__main__":
+
+
+    print("Kafka producer app started ...")
+    while True:
+        try:
+            kafka_producer = KafkaProducer(bootstrap_servers = KAFKA_BROKER,
+                                        value_serializer=lambda x: x.encode('utf-8'))
+            print("Connected to Kafka!")
+            break
+        except kafka.errors.NoBrokersAvailable as e:
+            print(e)
+            time.sleep(3)
+
+    cap = pyshark.LiveCapture("any")
+
+    for packet in cap.sniff_continuously():
+        try:
+            src_ip = packet.ip.src
+            dst_ip = packet.ip.dst
+
+            print(f"Source IP: {src_ip} --> Destination IP: {dst_ip}")
+            kafka_producer.send(KAFKA_TOPIC_NAME, bytes(f"Source IP: {src_ip} --> Destination IP: {dst_ip}", "utf-8"))
+            kafka_producer.flush()
+
+        except AttributeError as e:
+            print(f"Skipping packet: {e}")
+
