430a01174e062f228ef3cb5150597a88bb322d30
jelicm <jelicm2000@gmail.com>
2024-01-25 15:18:10 +0100

    stream queries

--- real-time/consumer/consumer.py
+++ real-time/consumer/consumer.py
@@ -2,6 +2,9 @@
 # /spark/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1 consumer.py
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import explode, split
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+from pyspark.sql.functions import *
+from pyspark.sql.window import Window
 
 def quiet_logs(sc):
     logger = sc._jvm.org.apache.log4j
@@ -22,15 +25,62 @@ rez = spark \
     .option("startingOffsets", "earliest") \
     .load()
 
-rez = rez.selectExpr("CAST(value AS STRING) as value")
 
-query = rez \
+schema = StructType([
+    StructField("src_ip", StringType(), True),
+    StructField("dst_ip", StringType(), True),
+    StructField("src_port", StringType(), True),
+    StructField("dst_port", StringType(), True),
+    StructField("protocol", StringType(), True),
+    StructField("length", StringType(), True),
+    
+])
+
+rez = rez.selectExpr("CAST(value AS STRING)") \
+    .select(from_json(col("value"), schema).alias("data")) \
+    .select("data.*")
+
+rez = rez.withColumn("received_timestamp", current_timestamp())
+
+#upit 1 -koliko razl src_ip je bilo u prethodnih 10 sekundi za tcp protokol
+query1_df = rez \
+    .filter(col("protocol") == "TCP") \
+    .groupBy(window(col("received_timestamp"), "10 seconds")) \
+    .agg(count("src_ip").alias("different_src_ip")) \
+    .select("window.start", "window.end", "different_src_ip")
+
+#upit 2 - ukupna veličina paketa pristiglih u poslednjih 10ak sekundi
+query2_df = rez \
+    .filter(col("protocol")=="TCP") \
+    .groupBy(window(col("received_timestamp"), "10 seconds")) \
+    .agg(sum("length").alias("sum_length")) \
+    .select("window.start", "window.end", "sum_length")
+
+
+#upit 3 -
+query3_df = rez \
+    .groupBy(window(col("received_timestamp"), "15 seconds")) \
+    .agg(count("protocol").alias("different_protocols")) \
+    .select("window.start", "window.end", "different_protocols")
+
+# upit 4 - nije gotov
+#query4_df = rez \
+#    .groupBy(window(col("received_timestamp"), "15 seconds").alias("tw")) \
+#    .agg(count("dst_ip").alias("different_ips")) \
+    
+
+
+#windowQuery4 = Window.partitionBy("start", "end").orderBy("different_ips")
+#query4_df = query4_df.withColumn("row_num", row_number().over(windowQuery4))
+#query4_df = query4_df.filter(col("row_num") == 1)
+
+
+query = query2_df \
     .writeStream \
-    .outputMode("append") \
+    .outputMode("complete") \
     .trigger(processingTime='10 seconds') \
     .format("console") \
-    .option("truncate", "false") \
     .start()
 
-
+#.option("truncate", "false") \
 query.awaitTermination()
--- real-time/producer/producer.py
+++ real-time/producer/producer.py
@@ -5,6 +5,7 @@ from kafka import KafkaProducer
 import kafka.errors
 import time
 import sys
+from json import dumps
 
 KAFKA_TOPIC_NAME = "network-data"
 KAFKA_BROKER = "localhost:9092"
@@ -20,7 +21,7 @@ if __name__ == "__main__":
     
         try:
             kafka_producer = KafkaProducer(bootstrap_servers = KAFKA_BROKER,
-                                        value_serializer=lambda x: x.encode('utf-8'))
+                                        value_serializer=lambda x: dumps(x).encode('utf-8'))
             print("Connected to Kafka!")
             sys.stdout.flush()
             break
@@ -35,11 +36,26 @@ if __name__ == "__main__":
         try:
             src_ip = packet.ip.src
             dst_ip = packet.ip.dst
-
-            print(f"Source IP: {src_ip} --> Destination IP: {dst_ip}")
+            transport_layer = packet.transport_layer
+            src_port = packet[transport_layer].srcport
+            dst_port = packet[transport_layer].dstport
+            protocol = packet.transport_layer
+            len = packet.length
+            value_dict = {
+                'src_ip': src_ip,
+                'dst_ip': dst_ip,
+                'src_port': src_port,
+                'dst_port': dst_port,
+                'protocol': protocol,
+                'length': len
+            }
+            #print(f"Source IP: {src_ip} --> Destination IP: {dst_ip}")
+            print(f"Source IP: {src_ip}, Source Port: {src_port}, Destination IP: {dst_ip}, Destination Port: {dst_port}, Protocol: {protocol}, Lwn {len}")
             sys.stdout.flush()
-            kafka_producer.send(KAFKA_TOPIC_NAME, value=f"Source IP: {src_ip} --> Destination IP: {dst_ip}")
+            #kafka_producer.send(KAFKA_TOPIC_NAME, value=f"Source IP: {src_ip} --> Destination IP: {dst_ip}")
+            kafka_producer.send(KAFKA_TOPIC_NAME, value=value_dict)
             kafka_producer.flush()
+            time.sleep(1)
 
         except AttributeError as e:
             print(f"Skipping packet: {e}")
